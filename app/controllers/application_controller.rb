7e35943c0ec8a3ad7bc512a51307dd7e39bc209f
Maximilian Rutyna <mrutyna777@gmail.com>
2016-06-02 14:55:54 -0700

    Finished ApplicationController- current_user, logged_in, login_user, logout_user, require_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,32 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user #2
+  helper_method :logged_in? #3
+
+
+  private #2
+
+  def current_user #2
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by_session_token(session[:session_token])
+  end
+
+  def logged_in? #3
+    !current_user.nil?
+  end
+
+  def login_user!(user) #4
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def logout_user! #5
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+
+  def require_user! #6
+    redirect_to new_session_url if current_user.nil?
+  end
 end
